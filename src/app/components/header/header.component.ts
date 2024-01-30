e72e135712396d77908722d6c9c493f455bc9ce5
Guillaume1979 <guibetguillaume@gmail.com>
2024-01-30 16:45:49 +0100

    Enhance header component and styles
    The header component is now updated with new dependencies and a 'layoutService' injection. Styles have been refined in 'styles.css' to improve the presentability of all elements. The static unordered list in the header's HTML file is replaced with the dynamically generating 'NavLink' component, helping ensure consistency and ease of maintenance.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {LayoutService} from "../../services/layout.service";
+import {NavLinkComponent} from "../shared-components/nav-link/nav-link.component";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NavLinkComponent,
+    NgOptimizedImage
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+layoutService = inject(LayoutService)
+  format = this.layoutService.format;
 }
