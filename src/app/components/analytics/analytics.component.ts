7a14da726c6a8a178ce2d607d57fad26a9ce3ef3
Guillaume1979 <guibetguillaume@gmail.com>
2024-02-06 15:57:41 +0100

    Add responsive layout change functionality to Analytics
    This commit introduces dynamic class assignment to the primary elements in the HTML structure of the Analytics component. It applies a grid layout using CSS, enabling responsive design based on different formats, such as 'Web'. It allows for flexible UI adjustment depending on the type

--- src/app/components/analytics/analytics.component.ts
+++ src/app/components/analytics/analytics.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {NgClass} from "@angular/common";
+import {LayoutService} from "../../services/layout.service";
 
 @Component({
   selector: 'app-analytics',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './analytics.component.html',
   styleUrl: './analytics.component.css'
 })
 export class AnalyticsComponent {
+  format = inject(LayoutService).format;
 
 }
